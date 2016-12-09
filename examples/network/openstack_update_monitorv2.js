99d25ee076a27230f1d3d1c3688dbc1b44635f10
jhihming.huang <jhihming.huang@hopebaytech.com>
2016-12-09 18:40:20 +0800

    finish health monitor

--- examples/network/openstack_update_monitorv2.js
+++ examples/network/openstack_update_monitorv2.js
@@ -1,6 +1,6 @@
 var pkgcloud = require('../../lib/pkgcloud');
-var getToken = require('./get_openstack_token');
 var KH_admin_tenantID = '6ade8ae8037b4e449a4c7c7a65dc5e1b';
+var getToken = require('./get_openstack_token');
 getToken.getToken(function (id){
   var client = pkgcloud.network.createClient({
     provider: 'openstack',
@@ -10,19 +10,19 @@ getToken.getToken(function (id){
     authUrl: 'http://172.16.31.1:35357',
     strictSSL: false
   });
-
-
   var options = {
-    'id': '3a3bd493-f532-4776-a5c2-f975b2b2ea91',
-    'description': 'updated loadbalancer',
-    'name': 'update name',
-    'admin_state_up': false,
+    'delay' : 10,
+    'expected_codes': '400,401',
+    'http_method': 'head',
+    'max_retries': 9,
+    'timeout': '13',
+    'url_path': '/update',
+    'id': '5f96c62f-71e6-4c9d-b397-c541f4fe2d69'
   };
-  client.updateLoadbalancer(options, function(err, lbs) {
+  client.updateHealthMonitorV2(options, function(err, listener) {
     if (err) {
       console.log(err);
-    } else {
-      console.log('success', lbs);
     }
+    console.log(listener);
   });
 });
